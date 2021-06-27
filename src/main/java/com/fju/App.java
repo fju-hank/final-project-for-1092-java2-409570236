5e8ed47541de5852b2a6599e9709d2287bd83f9b
409570236 <wush1237@gmail.com>
2021-06-27 12:08:09 +0800

    GUI 介面 -輸入使用者名稱 -確定

--- src/main/java/com/fju/App.java
+++ src/main/java/com/fju/App.java
@@ -1,28 +1,35 @@
 package com.fju;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App {
-    private JPanel panelain;
+    private JPanel panelMain;
+    private JPanel topPanel;
+    private JPanel buttomPanel;
+    private JTextField idText;
+    private JTextArea packPrint;
     private JButton 確認Button;
-    private JPanel panel1;
-    private JTextField textField1;
-    private JPanel panel2;
-    private JPanel panel3;
-    private JPanel panel4;
-    private JPanel panel5;
-    private JTextArea textArea1;
+    private JButton 取消Button;
+
+    public App() {
+        確認Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = idText.getText().trim();
+                packPrint.setText( id + "您好，歡迎使用。" );
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("App");
-        frame.setLocation(600, 350);
-        frame.setContentPane(new App().panelain);
+        frame.setLocation(450, 250);
+        frame.setContentPane(new App().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
     }
 
 
